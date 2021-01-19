300a824469e9e1a192571802d9bd415e347c0a96
emetdas <emet@b2bleadstaff.com>
2021-01-19 18:32:05 +0600

    some change

--- scroll into fixed nav/main.js
+++ scroll into fixed nav/main.js
@@ -1,34 +1,20 @@
-const btnScroll = document.querySelector(".btn-section-1");
-const section = document.querySelector("#section-one");
-
-btnScroll.addEventListener("click", function (e) {
-  // old way scrolling old way is supportiong old browser
-  const cordinat = section.getBoundingClientRect();
-  window.scrollTo({
-    left: cordinat.left + window.pageXOffset,
-    top: cordinat.top + window.pageYOffset,
-    behavior: "smooth",
-  });
-  // modern way
-  // section.scrollIntoView({behavior:'smooth'});
+const home = document.querySelector("#home");
+const nav = document.querySelector('nav');
+const conntainer = document.querySelector('.container');
+const conntainerHeight = conntainer.getBoundingClientRect().height;
+const callback = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) {
+  nav.classList.add('sticky');
+}
+else{
+  nav.classList.remove('sticky');
+}
+};
+const homeObserver = new IntersectionObserver(callback, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${conntainerHeight}px`,
 });
-// nav item target to scroll
-// bad way
-// const nav_link = document.querySelectorAll(".nav-link");
-// nav_link.forEach(function (el) {
-//   el.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const id = this.getAttribute("href");
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-//   });
-// });
-// right way
-const nav_item = document.querySelector(".nav-item");
-nav_item.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (e.target.classList.contains("nav-link")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
-  }
-});
+homeObserver.observe(home);
--- smooth scroll/main.js
+++ smooth scroll/main.js
@@ -1,1 +1,34 @@
+const btnScroll = document.querySelector(".btn-section-1");
+const section = document.querySelector("#section-one");
 
+btnScroll.addEventListener("click", function (e) {
+  // old way scrolling old way is supportiong old browser
+  const cordinat = section.getBoundingClientRect();
+  window.scrollTo({
+    left: cordinat.left + window.pageXOffset,
+    top: cordinat.top + window.pageYOffset,
+    behavior: "smooth",
+  });
+  // modern way
+  // section.scrollIntoView({behavior:'smooth'});
+});
+// nav item target to scroll
+// bad way
+// const nav_link = document.querySelectorAll(".nav-link");
+// nav_link.forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+// right way
+const nav_item = document.querySelector(".nav-item");
+nav_item.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  }
+});
