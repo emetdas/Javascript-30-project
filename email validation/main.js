6cd645988f92b458c3afe86cf4441304ca6b47d6
emetdas <emet@b2bleadstaff.com>
2021-04-12 12:41:03 +0600

    new update

--- email validation/main.js
+++ email validation/main.js
@@ -5,21 +5,29 @@ form.addEventListener("submit", fatch);
 function fatch(e) {
   e.preventDefault();
   var apikeymy = "2b36d9a766e74b408c97a8993802157b";
-  
+
   var email = document.querySelector(".email").value;
   var url = `https://emailvalidation.abstractapi.com/v1/?api_key=${apikeymy}&email=${email}`;
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function () {
     if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
       var result = JSON.parse(xmlHttp.responseText);
-      var show ='<ul>'+
-       '<li>'+result.quality_score+'</li>'+
-       '<li>'+result.is_valid_format[0]+'</li>'+
-       '<li>'+result.email+'</li>'+
-       '<li>'+result.deliverability+'</li>'
-      +'</ul>'
-      ; 
-      document.querySelector('.result').innerHTML = show;
+      var show =
+        "<ul>" +
+        "<li>" +
+        result.quality_score +
+        "</li>" +
+        "<li>" +
+        result.is_valid_format[0] +
+        "</li>" +
+        "<li>" +
+        result.email +
+        "</li>" +
+        "<li>" +
+        result.deliverability +
+        "</li>" +
+        "</ul>";
+      document.querySelector(".result").innerHTML = show;
     }
   };
   xmlHttp.open("GET", url, true); // true for asynchronous
@@ -43,3 +51,4 @@ function fatch(e) {
 
 // httpGetAsync(url);
 // });
+
