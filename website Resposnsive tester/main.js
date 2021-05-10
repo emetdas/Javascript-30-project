9740511ec0c18833ee26eef3e5da79ae1fc22dfe
emetdas <emet@b2bleadstaff.com>
2021-05-10 13:45:56 +0600

    url change regex

--- website Resposnsive tester/main.js
+++ website Resposnsive tester/main.js
@@ -35,7 +35,16 @@ let form = document.querySelector('.form-responsive-tester');
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     let input = document.querySelector('.url').value;
-    window.frames[0].location = input;
+    var RegExp =/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
+    var match = input.match(RegExp);
+    console.log(match);
+    if (match ||input !== null) {
+        window.frames[0].location = input;
+    }else{
+        return false;
+    }
+
 });
 // Test-any-website-end
 
