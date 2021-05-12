73a8614a55b9a74843ff5b74fa06ed1c2180aa38
emetdas <emet@b2bleadstaff.com>
2021-05-12 15:38:58 +0600

    update

--- comming soon timer/main.js
+++ comming soon timer/main.js
@@ -1,3 +1,4 @@
+let heading = document.querySelector('.heading');
 let year = document.querySelector('.year');
 let day = document.querySelector('.day');
 let hour = document.querySelector('.hour');
@@ -6,7 +7,7 @@ let second = document.querySelector('.second');
 
 let currentYear = new Date().getFullYear();
 let newYearTime = new Date(`january 01 ${currentYear + 1} 00:00:00`);
-
+heading.innerHTML = `New Year ${currentYear + 1}`
 function updateCounter() {
   let currentTime = new Date();
   let diff = newYearTime - currentTime;
@@ -14,11 +15,9 @@ function updateCounter() {
   let h = Math.floor((diff / 1000 / 60 / 60) % 24);
   let m = Math.floor((diff / 1000 / 60) % 60);
   let s = Math.floor((diff / 1000) % 60);
-
-  year.innerHTML = currentYear + 1;
   day.innerHTML = d;
   hour.innerHTML = h;
-  minutes.innerHTML = m;
+  minute.innerHTML = m;
   second.innerHTML = s;
 }
-setInterval (updateCounter(),1000);
+setInterval(updateCounter,1000);
