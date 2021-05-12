467ceee2eba43649ed16257fe583576df76fbf17
emetdas <emet@b2bleadstaff.com>
2021-05-12 15:21:47 +0600

    update

--- comming soon timer/main.js
+++ comming soon timer/main.js
@@ -6,7 +6,10 @@ let second = document.querySelector('.second');
 let currentYear = new Date().getFullYear();
 let newYearTime = new Date(`january 01 ${currentYear + 1} 00:00:00`); 
 
-
+function updateCounter(){
+    
+}
+updateCounter();
 
 
 
