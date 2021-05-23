3947988ef38a98e1bf2c2ec7b709845259c2cb6c
emetdas <emet@b2bleadstaff.com>
2021-05-23 17:11:23 +0600

    all update is done

--- geolocation api/main.js
+++ geolocation api/main.js
@@ -1,1 +1,14 @@
-navigator.geolocation.getCurrentPosition()
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude,longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
