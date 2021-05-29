320e088dc9a48cf6babb1bd1696cce79cf43f959
emetdas <emet@b2bleadstaff.com>
2021-05-29 17:53:23 +0600

    update

--- scroll to manue link active/main.js
+++ scroll to manue link active/main.js
@@ -1,9 +1,15 @@
 const sectionAll = document.querySelectorAll('section[id]');
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
+  const scrollY = window.pageYOffset;
 
+  sectionAll.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+
+    }
+  });
 });
-
-
-
-
